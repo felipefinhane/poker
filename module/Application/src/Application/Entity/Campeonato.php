f90bd6770f011c4f43876ea744685d06e934a099
Felipe F. de Paula <felipefinhane@gmail.com>
2016-10-31 10:13:05 -0200

    Atualização de composer e correção de codigo

--- module/Application/src/Application/Entity/Campeonato.php
+++ module/Application/src/Application/Entity/Campeonato.php
@@ -2,140 +2,140 @@
 
 namespace Application\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-
 /**
- * @ORM\Entity(repositoryClass="\Application\Entity\Repository\CampeonatoRepository") 
+ * @ORM\Entity(repositoryClass="\Application\Entity\Repository\CampeonatoRepository")
+ * @ORM\Table(name="campeonato")
  */
+
 class Campeonato {
 
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     * @ORM\Column(type="integer")
-     */
-    private $id_campeonato;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 * @ORM\Column(type="integer")
+	 */
+	private $id_campeonato;
 
-    /**
-     * @ORM\Column(type="string", length=45)
-     */
-    private $titulo;
+	/**
+	 * @ORM\Column(type="string", length=45)
+	 */
+	private $titulo;
 
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $descricao;
+	/**
+	 * @ORM\Column(type="text")
+	 */
+	private $descricao;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $data_inicio;
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $data_inicio;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $data_fim;
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $data_fim;
 
-    /**
-     * @ORM\Column(type="decimal")
-     */
-    private $valor_partida;
+	/**
+	 * @ORM\Column(type="decimal")
+	 */
+	private $valor_partida;
 
-    /**
-     * @ORM\Column(type="decimal")
-     */
-    private $porcentagem_campeonato;
-    
-    /**
-     * @ORM\OneToMany(targetEntity="Application\Entity\Partida", mappedBy="campeonato")
-     */
-    private $partida;
-    
-    /**
-     * @ORM\OneToMany(targetEntity="Application\Entity\CampeonatoPontuacao", mappedBy="campeonato")
-     */
-    private $campeonato_pontuacao;
+	/**
+	 * @ORM\Column(type="decimal")
+	 */
+	private $porcentagem_campeonato;
 
-    /**
-     * @ORM\OneToMany(targetEntity="Application\Entity\CampeonatoUsuario", mappedBy="campeonato")
-     */
-    private $campeonato_usuario;
+	/**
+	 * @ORM\OneToMany(targetEntity="Application\Entity\Partida", mappedBy="campeonato")
+	 */
+	private $partida;
 
-    public function setIdCampeonato($id_campeonato) {
-        $this->id_campeonato = $id_campeonato;
-        return $this;
-    }
+	/**
+	 * @ORM\OneToMany(targetEntity="Application\Entity\CampeonatoPontuacao", mappedBy="campeonato")
+	 */
+	private $campeonato_pontuacao;
 
-    public function getIdCampeonato() {
-        return $this->id_campeonato;
-    }
+	/**
+	 * @ORM\OneToMany(targetEntity="Application\Entity\CampeonatoUsuario", mappedBy="campeonato")
+	 */
+	private $campeonato_usuario;
 
-    public function setTitulo($titulo) {
-        $this->titulo = $titulo;
-        return $this;
-    }
+	public function setIdCampeonato($id_campeonato) {
+		$this->id_campeonato = $id_campeonato;
+		return $this;
+	}
 
-    public function getTitulo() {
-        return $this->titulo;
-    }
+	public function getIdCampeonato() {
+		return $this->id_campeonato;
+	}
 
-    public function setDescricao($descricao) {
-        $this->descricao = $descricao;
-        return $this;
-    }
+	public function setTitulo($titulo) {
+		$this->titulo = $titulo;
+		return $this;
+	}
 
-    public function getDescricao() {
-        return $this->descricao;
-    }
+	public function getTitulo() {
+		return $this->titulo;
+	}
 
-    public function setDataInicio($data_inicio) {
-        $dataInicio = \DateTime::createFromFormat('j/m/Y', $data_inicio);
-        $this->data_inicio = new \DateTime(date('Y-m-d H:i:s', strtotime($dataInicio->format('Y-m-d'))));
-        return $this;
-    }
+	public function setDescricao($descricao) {
+		$this->descricao = $descricao;
+		return $this;
+	}
 
-    public function getDataInicio() {
-        return $this->data_inicio->format('d/m/Y');
-    }
+	public function getDescricao() {
+		return $this->descricao;
+	}
 
-    public function setDataFim($data_fim) {
-        $dataFim = \DateTime::createFromFormat('j/m/Y', $data_fim);
-        $this->data_fim = new \DateTime(date('Y-m-d H:i:s', strtotime($dataFim->format('Y-m-d'))));
-        return $this;
-    }
+	public function setDataInicio($data_inicio) {
+		$dataInicio        = \DateTime::createFromFormat('j/m/Y', $data_inicio);
+		$this->data_inicio = new \DateTime(date('Y-m-d H:i:s', strtotime($dataInicio->format('Y-m-d'))));
+		return $this;
+	}
 
-    public function getDataFim() {
-        return $this->data_fim->format('d/m/Y');
-    }
+	public function getDataInicio() {
+		return $this->data_inicio->format('d/m/Y');
+	}
 
-    public function setValorPartida($valor_partida) {
-        $this->valor_partida = $valor_partida;
-        return $this;
-    }
+	public function setDataFim($data_fim) {
+		$dataFim        = \DateTime::createFromFormat('j/m/Y', $data_fim);
+		$this->data_fim = new \DateTime(date('Y-m-d H:i:s', strtotime($dataFim->format('Y-m-d'))));
+		return $this;
+	}
 
-    public function getValorPartida() {
-        return $this->valor_partida;
-    }
+	public function getDataFim() {
+		return $this->data_fim->format('d/m/Y');
+	}
 
-    public function setPorcentagemCampeonato($porcentagem_campeonato) {
-        $this->porcentagem_campeonato = $porcentagem_campeonato;
-        return $this;
-    }
+	public function setValorPartida($valor_partida) {
+		$this->valor_partida = $valor_partida;
+		return $this;
+	}
 
-    public function getPorcentagemCampeonato() {
-        return $this->porcentagem_campeonato;
-    }
-    
-    public function getArrayCopy() {
-        return [
-            'id_campeonato' => $this->id_campeonato,
-            'titulo' => $this->titulo,
-            'descricao' => $this->descricao,
-            'valor_partida' => $this->valor_partida,
-            'porcentagem_campeonato' => $this->porcentagem_campeonato,
-            'data_inicio' => $this->getDataInicio(),
-            'data_fim' => $this->getDataFim(),
-        ];
-    }
+	public function getValorPartida() {
+		return $this->valor_partida;
+	}
+
+	public function setPorcentagemCampeonato($porcentagem_campeonato) {
+		$this->porcentagem_campeonato = $porcentagem_campeonato;
+		return $this;
+	}
+
+	public function getPorcentagemCampeonato() {
+		return $this->porcentagem_campeonato;
+	}
+
+	public function getArrayCopy() {
+		return [
+			'id_campeonato'          => $this->id_campeonato,
+			'titulo'                 => $this->titulo,
+			'descricao'              => $this->descricao,
+			'valor_partida'          => $this->valor_partida,
+			'porcentagem_campeonato' => $this->porcentagem_campeonato,
+			'data_inicio'            => $this->getDataInicio(),
+			'data_fim'               => $this->getDataFim(),
+		];
+	}
 
 }
--- module/Application/src/Application/Entity/Partida.php
+++ module/Application/src/Application/Entity/Partida.php
@@ -2,70 +2,70 @@
 
 namespace Application\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-
 /**
- * @ORM\Entity(repositoryClass="\Application\Entity\Repository\PartidaRepository") 
+ * @ORM\Entity(repositoryClass="\Application\Entity\Repository\PartidaRepository")
+ * @ORM\Table(name="partida")
  */
+
 class Partida {
 
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     * @ORM\Column(type="integer")
-     */
-    private $id_partida;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 * @ORM\Column(type="integer")
+	 */
+	private $id_partida;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $data_inicio;
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $data_inicio;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $data_fim;
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $data_fim;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="Application\Entity\Campeonato", inversedBy="partida")
-     * @ORM\JoinColumn(name="id_campeonato",referencedColumnName="id_campeonato", nullable=false)
-     */
-    private $campeonato;
+	/**
+	 * @ORM\ManyToOne(targetEntity="Application\Entity\Campeonato", inversedBy="partida")
+	 * @ORM\JoinColumn(name="id_campeonato",referencedColumnName="id_campeonato", nullable=false)
+	 */
+	private $campeonato;
 
-    public function getIdPartida() {
-        return $this->id_partida;
-    }
+	public function getIdPartida() {
+		return $this->id_partida;
+	}
 
-    public function getDataInicio() {
-        return $this->data_inicio;
-    }
+	public function getDataInicio() {
+		return $this->data_inicio;
+	}
 
-    public function getDataFim() {
-        return $this->data_fim;
-    }
+	public function getDataFim() {
+		return $this->data_fim;
+	}
 
-    public function setIdPartida($id_partida) {
-        $this->id_partida = $id_partida;
-        return $this;
-    }
+	public function setIdPartida($id_partida) {
+		$this->id_partida = $id_partida;
+		return $this;
+	}
 
-    public function setDataInicio($data_inicio) {
-        $this->data_inicio = $data_inicio;
-        return $this;
-    }
+	public function setDataInicio($data_inicio) {
+		$this->data_inicio = $data_inicio;
+		return $this;
+	}
 
-    public function setDataFim($data_fim) {
-        $this->data_fim = $data_fim;
-        return $this;
-    }
-    
-    public function getCampeonato() {
-        return $this->campeonato;
-    }
+	public function setDataFim($data_fim) {
+		$this->data_fim = $data_fim;
+		return $this;
+	}
 
-    public function setCampeonato($campeonato) {
-        $this->campeonato = $campeonato;
-        return $this;
-    }
+	public function getCampeonato() {
+		return $this->campeonato;
+	}
+
+	public function setCampeonato($campeonato) {
+		$this->campeonato = $campeonato;
+		return $this;
+	}
 
 }
